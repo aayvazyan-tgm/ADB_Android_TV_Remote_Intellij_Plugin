33955d728647eda767ac89042f81871f8f56448b
Ari Ayvazyan <ariay@live.at>
2016-07-31 18:10:54 +0200

    Initial commit

---
+++ src/ToolWindowRemote.java
@@ -0,0 +1,18 @@
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * Created by AriAy on 31.07.2016.
+ */
+public class ToolWindowRemote implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        
+        toolWindow.getComponent().add(new JButton("TEST"));
+    }
+}
