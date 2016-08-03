8af57056d3b8deb395a50bc6cd8e6a53702d447d
Ari Ayvazyan <ariay@live.at>
2016-08-03 02:22:55 +0200

    finished core functionality

--- src/ToolWindowRemote.java
+++ src/ToolWindowRemote.java
@@ -6,44 +6,52 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 /**
  * Created by AriAy on 31.07.2016.
  */
 public class ToolWindowRemote implements ToolWindowFactory {
-    private JLabel connectedText;
+    private JLabel connectedInfoText;
+    private JTextField connectTo;
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         JPanel rootPanel = new JPanel(new VerticalFlowLayout());
 
-        JPanel controlsPanel = new JPanel(new BorderLayout());
-        controlsPanel.add(generateActionButton(22, "Enter"), BorderLayout.CENTER);
-        controlsPanel.add(generateActionButton(23, "^"), BorderLayout.NORTH);
-        controlsPanel.add(generateActionButton(24, "v"), BorderLayout.SOUTH);
-        controlsPanel.add(generateActionButton(25, "<"), BorderLayout.WEST);
-        controlsPanel.add(generateActionButton(26, ">"), BorderLayout.EAST);
 
-        connectedText = new JLabel("Not Connected");
+        JPanel controlsPanel = new JPanel(new BorderLayout());
+        controlsPanel.add(generateActionButton(23, "Enter"), BorderLayout.CENTER);
+        controlsPanel.add(generateActionButton(19, "^"), BorderLayout.NORTH);
+        controlsPanel.add(generateActionButton(20, "v"), BorderLayout.SOUTH);
+        controlsPanel.add(generateActionButton(21, "<"), BorderLayout.WEST);
+        controlsPanel.add(generateActionButton(22, ">"), BorderLayout.EAST);
+
+        this.connectTo = new JTextField("192.168.0.80");
+        connectedInfoText = new JLabel("Not Connected");
         JButton connectButton = new JButton("Connect");
         connectButton.addActionListener(e -> adbConnect());
         JButton refreshButton = new JButton("Refresh");
         refreshButton.addActionListener(e -> connectdCheck());
+
+        JPanel extraRow1 = new JPanel(new BorderLayout());
+        extraRow1.add(generateActionButton(4, "Back"), BorderLayout.EAST);
+        extraRow1.add(generateActionButton(3, "Home"), BorderLayout.WEST);
+
         //Fill the content Panel
-        rootPanel.add(connectedText);
+        rootPanel.add(connectTo);
+        rootPanel.add(connectedInfoText);
         rootPanel.add(connectButton);
         rootPanel.add(refreshButton);
 
         rootPanel.add(controlsPanel);
+        rootPanel.add(extraRow1);
         //Add the content to the toolwindow
         toolWindow.getComponent().add(rootPanel);
     }
 
     private void adbConnect() {
-        Utils.runCommand("adb connect 192.168.0.80");
+        Utils.runCommand("adb connect "+connectTo.getText());
         connectdCheck();
     }
 
@@ -53,30 +61,23 @@ public class ToolWindowRemote implements ToolWindowFactory {
             p.waitFor();
             String result = Utils.streamToString(p.getInputStream());
             String[] resultLines = result.split("\\r?\\n");
-            boolean connected=false;
+            boolean connected = false;
             for (String line : resultLines) {
-                if(line.contains("no device")|line.contains("offline"))continue;
-                    if(line.contains("\tdevice")){
-                    this.connectedText.setText("Connected to: "+line.substring(0,line.indexOf("\t")));
-                    connected=true;
+                if (line.contains("no device") | line.contains("offline")) continue;
+                if (line.contains("\tdevice")) {
+                    this.connectedInfoText.setText("Connected to: " + line.substring(0, line.indexOf("\t")));
+                    connected = true;
                 }
             }
-            if(connected==false)this.connectedText.setText("Not connected");
+            if (connected == false) this.connectedInfoText.setText("Not connected");
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private JButton generateActionButton(int androidKeyEventID, String displayText) {
+    private JButton generateActionButton(final int androidKeyEventID, String displayText) {
         JButton button = new JButton(displayText);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (e.getID() == ActionEvent.ACTION_PERFORMED) {
-                    Utils.runCommand("adb input KeyEvent " + androidKeyEventID);
-                }
-            }
-        });
+        button.addActionListener(e -> Utils.runCommand("adb shell input keyevent " + androidKeyEventID));
         return button;
     }
 }
